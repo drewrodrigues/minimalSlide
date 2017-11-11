25045cc9b0a397ceacc302f9329bd489cba3ba15
Andrew <thesimpledev@gmail.com>
2017-11-11 13:28:02 -0800

    clean up code

--- js/minimalSlide.js
+++ js/minimalSlide.js
@@ -1,24 +1,46 @@
 var minimalSlide = {
 	// setup -------------------------------------------
-	// setupControls(count): adds list items to ul.mSControls based on slidesCount
 	// setupSlides(): assigns slides & slidesCount, and adds active class to first slide
+	// setupControls(): adds list items to .mSContainer based on slidesCount
+	// toggleSlideshowOnButtonClick(): sets up event listener for play/pause button
+	// changeSlideOnControlClick(): sets up event listener for controls
 
 	// methods -----------------------------------------
-	// activeSlide(): returns the current slide with .active
-	// activeSlideIndex(): returns the index of the slide with .active
 	// startSlideshow(): starts slideshow from current active slide
 	// stopSlideshow(): stops slideshow at current active slide
 	// nextSlide(): transitions to the next slide
 	// prevSlide(): transitions back a slide
+	// goToSlide(index): transitions to the slide at index
 
 	// properties --------------------------------------
-	// slides: contains slides with .mS
-	// controls: contains list items that are children of .mSControls
-	// slidesCount: returns the amount of slides
+	// activeSlide: active slide, which is visible
+	// activeSlide: index of the active slide
+	// slides: slides which are found by .mSSlide  
+	// controls: slideshow circle controls 
+	// slidesCount: amount of slides
 	// animation: holds the setInterval call
-	// controlBackgroundColor:
-	// controlActiveColor:
-	// currentControlColor: returns the color within the controlColor attribute
+	// currentControlColor: color within the active slide's controlColor attribute
+
+	// settings --------------------------------------
+	// controlDefaultActiveColor: sets the default active control color
+	// controlBackgroundColor: sets the background color of the controls
+	// autoPlay: if set to false, the slideshow will be paused upon page load
+	// delayTime: sets the delay time between slide transitions in ms, ex: 5000 = 5 seconds
+	// transitionTime: sets the transition time in ms, ex: 1000 = 1 second
+	// controlGradient: when set to false, control background gradient will be removed
+
+	// setup -------------------------------------------
+	setupSlides: function() {
+		this.slides = $('.mSSlide');
+		this.slides
+			.hide()
+			.first()
+			.addClass('active')
+			.fadeIn();
+		this.slidesCount = this.slides.length;
+		this.activeSlide = this.slides.filter('.active');
+		this.activeSlideIndex = this.activeSlide.index();
+	},
 
 	setupControls: function() {
 		// add controls based on the amount of slides
@@ -29,25 +51,14 @@ var minimalSlide = {
 		}
 		$container.append($ul);
 		this.controls = $ul.children('li');
+		this.currentControlColor = this.activeSlide.attr('controlColor');
 		this.controls
 			.first()
 			.addClass('control-active')
-			.css('backgroundColor', this.currentControlColor());
+			.css('backgroundColor', this.currentControlColor);
 		this.toggle = $('.mSToggle');
 		this.changeSlideOnControlClick();
 		this.toggleSlideshowOnButtonClick();
-	},
-
-	currentControlColor: function() {
-		return this.activeSlide.attr('controlColor');
-	},
-
-	changeSlideOnControlClick: function() {
-		self = this;
-		this.controls.on('click', function() {
-			self.stopSlideshow();
-			self.goToSlide($(this).index() - 1);
-		});
 	},
 
 	toggleSlideshowOnButtonClick: function() {
@@ -62,35 +73,38 @@ var minimalSlide = {
 		});
 	},
 
-	setupSlides: function() {
-		this.slides = $('.mSSlide');
-		this.slides
-			.hide()
-			.first()
-			.addClass('active')
-			.fadeIn();
-		this.slidesCount = this.slides.length;
-		this.activeSlide = this.slides.filter('.active');
-		this.activeSlideIndex = this.activeSlide.index();
+	changeSlideOnControlClick: function() {
+		self = this;
+		this.controls.on('click', function() {
+			self.stopSlideshow();
+			self.goToSlide($(this).index() - 1);
+		});
+	},
+	
+	setup: function() {
+		this.setupSlides();
+		this.setupControls();
+		this.startSlideshow();
 	},
 
-	onFirstSlide: function() {
-		return this.activeSlideIndex === 0;
-	},
-
-	onLastSlide: function() {
-		return this.activeSlideIndex === this.slides.length - 1;
-	},
-
-	updateActiveControl: function(index) {
+	// property updating methods -----------------------------------------
+	updateActiveControl: function() {
+		this.currentControlColor = this.activeSlide.attr('controlColor');
 		this.controls
 			.removeClass('control-active')
 			.css('backgroundColor', 'white')
-			.eq(index)
+			.eq(this.activeSlideIndex)
 			.addClass('control-active')
-			.css('backgroundColor', this.currentControlColor());
+			.css('backgroundColor', this.currentControlColor);
 	},
 
+	changeActiveSlideTo: function(index) {
+		this.activeSlide.removeClass('active');
+		this.activeSlide = this.slides.eq(index).addClass('active');
+		this.activeSlideIndex = index;
+	},
+
+	// slide actions -----------------------------------------
 	nextSlide: function() {
 		if (this.onLastSlide()) {
 			this.goToSlide(0);
@@ -106,7 +120,18 @@ var minimalSlide = {
 			this.goToSlide(this.activeSlideIndex - 1);
 		}
 	},
+	
+	goToSlide: function(index) {
+		self = this;
+		self.slides.finish();
+		this.changeActiveSlideTo(index);
+		this.updateActiveControl();
+		this.activeSlide.fadeIn(1000, function() {
+			self.slides.not('.active').hide();
+		});
+	},
 
+	// slideshow actions -----------------------------------------
 	startSlideshow: function() {
 		var self = this;
 		this.toggle.text('Pause');
@@ -120,27 +145,14 @@ var minimalSlide = {
 		clearInterval(this.animate);
 	},
 
-	goToSlide: function(index) {
-		self = this;
-		self.slides.finish();
-		this.changeActiveSlideTo(index);
-		this.activeSlide.fadeIn(1000, function() {
-			self.slides.not('.active').hide();
-		});
-		this.updateActiveControl(index);
+	// abstractions -----------------------------------------
+	onFirstSlide: function() {
+		return this.activeSlideIndex === 0;
 	},
 
-	changeActiveSlideTo: function(index) {
-		this.activeSlide.removeClass('active');
-		this.activeSlide = this.slides.eq(index).addClass('active');
-		this.activeSlideIndex = index;
-	},
-
-	setup: function() {
-		this.setupSlides();
-		this.setupControls();
-		this.startSlideshow();
+	onLastSlide: function() {
+		return this.activeSlideIndex === this.slides.length - 1;
 	}
-}
+};
 
 minimalSlide.setup();
